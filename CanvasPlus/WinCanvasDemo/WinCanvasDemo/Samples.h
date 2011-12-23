a2d2224d0ba4464c25f2bb1e9f8ccf53bf8a6e34
thiago.adams <thiago.adams@fdec673f-17c4-7d02-b730-e9e86fb7c04d>
2011-12-23 19:21:17 +0000

    New demo, new folder, new file name

---
+++ CanvasPlus/WinCanvasDemo/WinCanvasDemo/Samples.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <Canvas.h>
+
+void Sample1(CanvasPlus::Canvas&);
+void Sample2(CanvasPlus::Canvas&);
+void Sample3(CanvasPlus::Canvas&);
---
+++ CanvasPlus/WinCanvasDemo/WinCanvasDemo/WinCanvasDemo.h
@@ -0,0 +1,62 @@
+#pragma once
+
+// Windows Header Files:
+#include <windows.h>
+
+// C RunTime Header Files:
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <wchar.h>
+#include <math.h>
+
+
+#ifndef Assert
+#if defined( DEBUG ) || defined( _DEBUG )
+#define Assert(b) do {if (!(b)) {OutputDebugStringA("Assert: " #b "\n");}} while(0)
+#else
+#define Assert(b)
+#endif //DEBUG || _DEBUG
+#endif
+
+#ifndef HINST_THISCOMPONENT
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
+#endif
+
+//
+// DemoApp class declaration
+//
+class DemoApp
+{
+public:
+    DemoApp();
+    ~DemoApp();
+
+    // Register the window class and call methods for instantiating drawing resources
+    HRESULT Initialize();
+
+    // Process and dispatch messages
+    void RunMessageLoop();
+
+private:
+  
+    // Resize the render target.
+    void OnResize(
+        UINT width,
+        UINT height
+        );
+
+    // The windows procedure.
+    static LRESULT CALLBACK WndProc(HWND hWnd,
+        UINT message,
+        WPARAM wParam,
+        LPARAM lParam
+        );
+private:
+    HWND m_hwnd;  
+    static const int TOTALSAMPLES = 3;
+    int m_CurrentSampleIndex;;
+};
+
+
