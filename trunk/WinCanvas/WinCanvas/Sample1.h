eb5489ae7200f60a24cfb15899f8575f35e6ece4
thiago.adams <thiago.adams@fdec673f-17c4-7d02-b730-e9e86fb7c04d>
2011-12-23 19:22:44 +0000

    old folder deleted

--- trunk/WinCanvas/WinCanvas/Sample1.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "CanvasRenderingContext2D.h"
-
-
-
-void Sample1(CanvasPlus::Canvas& canvas);
-void Sample2(CanvasPlus::Canvas& canvas);
-void Sample3(CanvasPlus::Canvas& canvas);
