4ad6fcb80bbce6affe2aa9651e77f90e5cd1762d
thiago.adams <thiago.adams@fdec673f-17c4-7d02-b730-e9e86fb7c04d>
2011-12-23 14:13:17 +0000

    Fixed font align (test program included)

--- trunk/WinCanvas/WinCanvas/Win_CanvasRenderingContext2D.cpp
+++ trunk/WinCanvas/WinCanvas/Win_CanvasRenderingContext2D.cpp
@@ -1,6 +1,7 @@
 #include "StdAfx.h"
 #include "CanvasRenderingContext2D.h"
 #include <Windows.h>
+#include <cassert>
 
 //THIS FILE IS WINDOWS ONLY AND GDI ONLY
 //GDI+ at the same file?
@@ -21,7 +22,7 @@ static void FillSolidRect(HDC hDC, LPCRECT lpRect, COLORREF clr)
 
 CanvasPlus::Font::Font()
 {
-    //"When the context is created, the font of the 
+    //"When the context is created, the font of the
     //context must be set to 10px sans-serif. "
     LOGFONT logFont;
     logFont.lfHeight = -10; //-17;
@@ -60,7 +61,7 @@ CanvasPlus::CanvasRenderingContext2D& CanvasPlus::Canvas::getContext(const char*
 }
 
 CanvasPlus::CanvasRenderingContext2D::CanvasRenderingContext2D(void* p)
-    : m_pNativeHandle(p)      
+    : m_pNativeHandle(p)
 {
     //"When the context is created, the lineWidth attribute must initially have the value 1.0."
     lineWidth = 1.0;
@@ -94,15 +95,14 @@ void CanvasPlus::CanvasRenderingContext2D::strokeRect(double x, double y, double
     RECT rect = {(LONG)x, (LONG)y, (LONG)(x + w), (LONG)(y + h)};
     COLORREF color = RGB(fillStyle.m_Color.r, fillStyle.m_Color.g, fillStyle.m_Color.b);
     //DrawRect(hdc, &rect, color);
-
-  HPEN hpen = CreatePen(PS_SOLID, lineWidth, color); //TODO
-  HPEN oldPen = (HPEN) SelectObject(hdc, hpen);
-  HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, GetStockObject(HOLLOW_BRUSH));
-  //
-  ::Rectangle(hdc, (LONG)x, (LONG)y, (LONG)(x + w), (LONG)(y + h));
-  //
-  SelectObject(hdc, oldPen);
-  SelectObject(hdc, oldBrush );
+    HPEN hpen = CreatePen(PS_SOLID, lineWidth, color); //TODO
+    HPEN oldPen = (HPEN) SelectObject(hdc, hpen);
+    HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, GetStockObject(HOLLOW_BRUSH));
+    //
+    ::Rectangle(hdc, (LONG)x, (LONG)y, (LONG)(x + w), (LONG)(y + h));
+    //
+    SelectObject(hdc, oldPen);
+    SelectObject(hdc, oldBrush);
 }
 
 
@@ -119,103 +119,111 @@ int CanvasPlus::CanvasRenderingContext2D::ToPixelY(double y)
     return (int) y;
 }
 
-UINT TextAlignToWindowsFlags(CanvasPlus::TextAlign textAlign,
-                             CanvasPlus::TextBaseline textBaseline)
-{
-    UINT flags = 0;
-
-    switch (textAlign)
-    {
-        case CanvasPlus::TextAlignStart:
-            flags |= TA_LEFT;
-            break;//?
-
-        case CanvasPlus::TextAlignEnd:
-            flags |= TA_LEFT;
-            break;//?
-
-        case CanvasPlus::TextAlignLeft:
-            flags |= TA_LEFT;
-            break;//?
-
-        case CanvasPlus::TextAlignRight:
-            flags |= TA_RIGHT;
-            break;//?
-
-        case CanvasPlus::TextAlignCenter:
-            flags |= TA_CENTER;
-            break;//?
-    }
-
-    switch (textBaseline)
-    {
-        case CanvasPlus::TextBaselineTop :
-            flags |= TA_TOP;
-            break;//?  //The top of the em square
-
-        case CanvasPlus::TextBaselineHanging:
-            break; //The hanging baseline
-
-        case CanvasPlus::TextBaselineMiddle :
-            break; //The middle of the em square
-
-        case CanvasPlus::TextBaselineAlphabetic :
-            flags |= TA_BASELINE ;
-            break;//The alphabetic baseline
-
-        case CanvasPlus::TextBaselineIdeographic :
-            break; //The ideographic baseline
-
-        case CanvasPlus::TextBaselineBottom :
-            flags |= TA_BOTTOM;
-            break;//? //The bottom of the em square
-
-        default:
-            break;
-    }
-
-    return flags;
-}
 
 CanvasPlus::TextMetrics CanvasPlus::CanvasRenderingContext2D::measureText(const wchar_t* psz)
 {
-  //------------------------------
+    //------------------------------
     HDC hdc = (HDC) m_pNativeHandle;
     //-------------------------------
-
-  //  TEXTMETRIC tm;
-  //  GetTextMetrics(hdc, &tm);
-
-  SIZE sz;
-  ::GetTextExtentPoint(hdc, psz, wcslen(psz), &sz);
-  return TextMetrics(sz.cx);
+    //  TEXTMETRIC tm;
+    //  GetTextMetrics(hdc, &tm);
+    SIZE sz;
+    ::GetTextExtentPoint(hdc, psz, wcslen(psz), &sz);
+    return TextMetrics(sz.cx);
 }
 
 void CanvasPlus::CanvasRenderingContext2D::moveTo(double x, double y)
 {
-  //------------------------------
-  HDC hdc = (HDC) m_pNativeHandle;
-  //-------------------------------
-  const int ix = ToPixelX(x);
-  const int iy = ToPixelY(y);
-  ::MoveToEx(hdc, ix, iy, NULL);
+    //------------------------------
+    HDC hdc = (HDC) m_pNativeHandle;
+    //-------------------------------
+    const int ix = ToPixelX(x);
+    const int iy = ToPixelY(y);
+    ::MoveToEx(hdc, ix, iy, NULL);
 }
 
 void CanvasPlus::CanvasRenderingContext2D::lineTo(double x, double y)
 {
-  //------------------------------
-  HDC hdc = (HDC) m_pNativeHandle;
-  //-------------------------------
-  const int ix = ToPixelX(x);
-  const int iy = ToPixelY(y);
+    //------------------------------
+    HDC hdc = (HDC) m_pNativeHandle;
+    //-------------------------------
+    const int ix = ToPixelX(x);
+    const int iy = ToPixelY(y);
+    COLORREF color = RGB(fillStyle.m_Color.r, fillStyle.m_Color.g, fillStyle.m_Color.b);
+    HPEN hpen = CreatePen(PS_SOLID, lineWidth, color); //TODO
+    HPEN oldPen = (HPEN) SelectObject(hdc, hpen);
+    //
+    ::LineTo(hdc, ix, iy);
+    //
+    SelectObject(hdc, oldPen);
+}
 
-  COLORREF color = RGB(fillStyle.m_Color.r, fillStyle.m_Color.g, fillStyle.m_Color.b);
-  HPEN hpen = CreatePen(PS_SOLID, lineWidth, color); //TODO
-  HPEN oldPen = (HPEN) SelectObject(hdc, hpen);
-  //
-  ::LineTo(hdc, ix, iy);
-  //
-  SelectObject(hdc, oldPen);
+static void GdiTextOut(HDC hdc,
+                       CanvasPlus::TextAlign textAlign,
+                       CanvasPlus::TextBaselineEnum baseLine,
+                       const wchar_t* psz,
+                       int textlen,
+                       int ix,
+                       int iy)
+{
+    assert(GetTextAlign(hdc) == TA_LEFT);
+
+    SIZE sz;
+    ::GetTextExtentPointW(hdc, psz, textlen, &sz);
+
+    int newx = ix;
+    int newy = iy;
+
+    switch (textAlign)
+    {
+    case CanvasPlus::TextAlignStart:  newx = ix;          break;
+    case CanvasPlus::TextAlignEnd:    newx = ix - sz.cx;  break;
+    case CanvasPlus::TextAlignLeft:   newx = ix;          break;
+    case CanvasPlus::TextAlignRight:  newx = ix - sz.cx;  break;
+    case CanvasPlus::TextAlignCenter: newx = ix - sz.cx / 2; break;
+    default:
+        assert(false);
+    }
+
+    switch (baseLine)
+    {
+    case CanvasPlus::TextBaselineTop :
+        newy = iy;
+        break;//The top of the em square
+
+    case CanvasPlus::TextBaselineHanging:
+        newy = iy;
+        break; //The hanging baseline
+
+    case CanvasPlus::TextBaselineMiddle :
+        newy = iy - sz.cy / 2;
+        break; //The middle of the em square
+
+    case CanvasPlus::TextBaselineAlphabetic :
+        {
+            TEXTMETRIC tm;
+            GetTextMetrics(hdc, &tm);
+            newy = iy - (tm.tmHeight - tm.tmDescent);
+        }
+        break;//The alphabetic baseline
+
+    case CanvasPlus::TextBaselineIdeographic :
+        {
+            TEXTMETRIC tm;
+            GetTextMetrics(hdc, &tm);
+            newy = iy - (tm.tmHeight);
+        }
+        break; //The ideographic baseline
+
+    case CanvasPlus::TextBaselineBottom :
+        newy = iy - sz.cy;
+        break;//The bottom of the em square
+
+    default:
+        break;
+    }
+
+    TextOut(hdc, newx, newy, psz, wcslen(psz));
 }
 
 void CanvasPlus::CanvasRenderingContext2D::fillText(const wchar_t* psz, double x, double y)
@@ -226,16 +234,13 @@ void CanvasPlus::CanvasRenderingContext2D::fillText(const wchar_t* psz, double x
     const int ix = ToPixelX(x);
     const int iy = ToPixelY(y);
     //
-    UINT flags = TextAlignToWindowsFlags(this->textAlign, this->textBaseline);
-    UINT old = SetTextAlign(hdc, flags);
     COLORREF color = RGB(fillStyle.m_Color.r, fillStyle.m_Color.g, fillStyle.m_Color.b);
     COLORREF oldcolor = SetTextColor(hdc, color);
     int oldbkmode = SetBkMode(hdc, TRANSPARENT);
     HFONT oldfont = (HFONT) SelectObject(hdc, (HFONT)font.m_pNativeObject);
     //
-    TextOut(hdc, ix, iy, psz, wcslen(psz));
+    GdiTextOut(hdc, this->textAlign, this->textBaseline, psz, wcslen(psz), ix, iy);
     //
-    SetTextAlign(hdc, old);
     SetTextColor(hdc, oldcolor);
     SetBkMode(hdc, oldbkmode);
     SelectObject(hdc, oldfont);
