fb35e21594a096ded5e4a3767ab051a07368900a
thiago.adams <thiago.adams@fdec673f-17c4-7d02-b730-e9e86fb7c04d>
2011-12-23 22:02:40 +0000

    some limited gradients with GDI added. (just to allow fillrects at this point)

--- CanvasPlus/CanvasPlus/Canvas.cpp
+++ CanvasPlus/CanvasPlus/Canvas.cpp
@@ -1,135 +1,159 @@
 
 #include "Canvas.h"
-#include <Windows.h>
+
 #include <string>
 #include <sstream>
-//#include "CanvasParserDFA.h"
 
-
-CanvasPlus::Color::Color(const char*)
+namespace CanvasPlus
 {
-}
 
-static CanvasPlus::Color backcolor = "rgb(255,0,0)";
-
-
-int ParsePositiveInt(const wchar_t* psz)
-{
-  int result = 0; 
-  while ((*psz >= '0') && (*psz <= '9'))
-  { 
-    result = result * 10 + (*psz - '0'); 
-    psz++; 
-  } 
-  return result;
-}
-
-
-int ParseAndMovePositiveInt(const char*& psz)
-{
-  int result = 0; 
-  while ((*psz >= '0') && (*psz <= '9'))
-  { 
-    result = result * 10 + (*psz - '0'); 
-    psz++; 
-  } 
-  return result;
-}
-
-
-static bool Skip(const char*& psz)
-{
-   //"rgb()"
-   while ((*psz < '0') || (*psz > '9'))
+    Color::Color(const char* psz)
     {
-        if(*psz == 0)
-            return false; //terminou
-        psz++;
+        ParserColor(psz, *this);
     }
-   return true;
-}
-void CanvasPlus::ParserColor(const char* psz, CanvasPlus::Color& color)
-{
-  if (!Skip(psz))
-      return;
-    
-  color.r = ParseAndMovePositiveInt(psz);
 
-  if (!Skip(psz))
-      return;
-
-  color.g = ParseAndMovePositiveInt(psz);
-
-  if (!Skip(psz))
-      return;
-
-  color.b = ParseAndMovePositiveInt(psz);
-
-  if (!Skip(psz))
-      return;
-  
-  color.a = ParseAndMovePositiveInt(psz);
-
-  if (!Skip(psz))
-      return;
-}
+    static Color backcolor = "rgb(255,0,0)";
 
 
-CanvasPlus::TextBaselineEnum CanvasPlus::ParseTextBaseline(const char* psz)
-{  
-  if (strcmp(psz, "top") == 0)
-  {
-    return CanvasPlus::TextBaselineTop;
-  }
-  else if (strcmp(psz, "hanging") == 0)
-  {
-    return CanvasPlus::TextBaselineHanging;
-  }
-  else if (strcmp(psz, "middle") == 0)
-  {
-    return CanvasPlus::TextBaselineMiddle;
-  }
-  else if (strcmp(psz, "alphabetic") == 0)
-  {
-    return CanvasPlus::TextBaselineAlphabetic;
-  }
-  else if (strcmp(psz, "ideographic") == 0)
-  {
-    return CanvasPlus::TextBaselineIdeographic;
-  }
-  else if (psz == "bottom" || strcmp(psz, "bottom") == 0)
-  {
-    return CanvasPlus::TextBaselineBottom;
-  }
+    int ParsePositiveInt(const wchar_t* psz)
+    {
+        int result = 0;
 
-  //assert
-  return CanvasPlus::TextBaselineTop;
-}
+        while ((*psz >= '0') && (*psz <= '9'))
+        {
+            result = result * 10 + (*psz - '0');
+            psz++;
+        }
+
+        return result;
+    }
 
 
- CanvasPlus::TextAlignEnum CanvasPlus::ParseTextAlign(const char* psz)
- {
-    if (strcmp(psz, "start") == 0)
-  {
-    return CanvasPlus::TextAlignStart;
-  }
-  else if (strcmp(psz, "end") == 0)
-  {
-    return CanvasPlus::TextAlignEnd;
-  }
-     else if (strcmp(psz, "left") == 0)
-  {
-    return CanvasPlus::TextAlignLeft;
-  }
-   else if (strcmp(psz, "right") == 0)
-  {
-    return CanvasPlus::TextAlignRight;
-  }
-   else if (strcmp(psz, "center") == 0)
-  {
-    return CanvasPlus::TextAlignCenter;
-  }
+    int ParseAndMovePositiveInt(const char*& psz)
+    {
+        int result = 0;
 
-    //assert
-    return CanvasPlus::TextAlignLeft;
- }
+        while ((*psz >= '0') && (*psz <= '9'))
+        {
+            result = result * 10 + (*psz - '0');
+            psz++;
+        }
+
+        return result;
+    }
+
+
+    static bool Skip(const char*& psz)
+    {
+        //"rgb()"
+        while ((*psz < '0') || (*psz > '9'))
+        {
+            if (*psz == 0)
+            {
+                return false;    //terminou
+            }
+
+            psz++;
+        }
+
+        return true;
+    }
+    void ParserColor(const char* psz, Color& color)
+    {
+        if (!Skip(psz))
+        {
+            return;
+        }
+
+        color.r = ParseAndMovePositiveInt(psz);
+
+        if (!Skip(psz))
+        {
+            return;
+        }
+
+        color.g = ParseAndMovePositiveInt(psz);
+
+        if (!Skip(psz))
+        {
+            return;
+        }
+
+        color.b = ParseAndMovePositiveInt(psz);
+
+        if (!Skip(psz))
+        {
+            return;
+        }
+
+        color.a = ParseAndMovePositiveInt(psz);
+
+        if (!Skip(psz))
+        {
+            return;
+        }
+    }
+
+
+
+    TextBaselineEnum ParseTextBaseline(const char* psz)
+    {
+        if (strcmp(psz, "top") == 0)
+        {
+            return TextBaselineTop;
+        }
+        else if (strcmp(psz, "hanging") == 0)
+        {
+            return TextBaselineHanging;
+        }
+        else if (strcmp(psz, "middle") == 0)
+        {
+            return TextBaselineMiddle;
+        }
+        else if (strcmp(psz, "alphabetic") == 0)
+        {
+            return TextBaselineAlphabetic;
+        }
+        else if (strcmp(psz, "ideographic") == 0)
+        {
+            return TextBaselineIdeographic;
+        }
+        else if (psz == "bottom" || strcmp(psz, "bottom") == 0)
+        {
+            return TextBaselineBottom;
+        }
+
+        //assert
+        return TextBaselineTop;
+    }
+
+
+    TextAlignEnum ParseTextAlign(const char* psz)
+    {
+        if (strcmp(psz, "start") == 0)
+        {
+            return TextAlignStart;
+        }
+        else if (strcmp(psz, "end") == 0)
+        {
+            return TextAlignEnd;
+        }
+        else if (strcmp(psz, "left") == 0)
+        {
+            return TextAlignLeft;
+        }
+        else if (strcmp(psz, "right") == 0)
+        {
+            return TextAlignRight;
+        }
+        else if (strcmp(psz, "center") == 0)
+        {
+            return TextAlignCenter;
+        }
+
+        //assert
+        return TextAlignLeft;
+    }
+
+} //CanvasPlus
+
--- CanvasPlus/CanvasPlus/Win/Win_Canvas.cpp
+++ CanvasPlus/CanvasPlus/Win/Win_Canvas.cpp
@@ -9,6 +9,62 @@
 
 //TODO Direct2d
 
+//Gradient
+#pragma comment( lib, "msimg32.lib" )
+///
+
+static void GradientRectH(HDC hDC, RECT& rect, COLORREF fore, COLORREF bk)
+{
+    TRIVERTEX v[2];
+    GRADIENT_RECT r[1];
+    v[0].x     = (rect.left);
+    v[0].y     = (rect.top);
+    v[0].Red   = GetRValue(bk) << 8;
+    v[0].Green = GetGValue(bk) << 8;
+    v[0].Blue  = GetBValue(bk) << 8;
+    v[0].Alpha = 0x0000;
+    v[1].x     = (rect.right);
+    v[1].y     = (rect.bottom);
+    v[1].Red   = GetRValue(fore) << 8;
+    v[1].Green = GetGValue(fore) << 8;
+    v[1].Blue  = GetBValue(fore) << 8;
+    v[1].Alpha = 0x0000;
+    r[0].UpperLeft = 0;
+    r[0].LowerRight = 1;
+    GradientFill(hDC, v, (ULONG)2, r, (ULONG)1, GRADIENT_FILL_RECT_H);
+}
+
+static void FillGradientRect(HDC hDC, RECT& rect, COLORREF bk, COLORREF fore,  ULONG dwMode)
+{
+    TRIVERTEX v[2];
+    GRADIENT_RECT r[1];
+    v[0].x     = (rect.left);
+    v[0].y     = (rect.top);
+    v[0].Red   = GetRValue(bk) << 8;
+    v[0].Green = GetGValue(bk) << 8;
+    v[0].Blue  = GetBValue(bk) << 8;
+    v[0].Alpha = 0x0000;
+    v[1].x     = (rect.right);
+    v[1].y     = (rect.bottom);
+    v[1].Red   = GetRValue(fore) << 8;
+    v[1].Green = GetGValue(fore) << 8;
+    v[1].Blue  = GetBValue(fore) << 8;
+    v[1].Alpha = 0x0000;
+    r[0].UpperLeft = 0;
+    r[0].LowerRight = 1;
+    GradientFill(hDC, v, (ULONG)2, r, (ULONG)1, dwMode);
+}
+
+//, GRADIENT_FILL_RECT_H
+static void GradientRectV(HDC hdc, RECT& rect, COLORREF fore, COLORREF bk, ULONG dwMode = GRADIENT_FILL_RECT_V)
+{
+    FillGradientRect(hdc, rect, bk, fore, GRADIENT_FILL_RECT_V);
+}
+
+static void GradientRectH(HDC hdc, RECT& rect, COLORREF fore, COLORREF bk, ULONG dwMode = GRADIENT_FILL_RECT_V)
+{
+    FillGradientRect(hdc, rect, fore, bk, GRADIENT_FILL_RECT_H);
+}
 
 static void FillSolidRect(HDC hDC, LPCRECT lpRect, COLORREF clr)
 {
@@ -21,186 +77,342 @@ static void FillSolidRect(HDC hDC, LPCRECT lpRect, COLORREF clr)
     }
 }
 
-CanvasPlus::Font::Font()
+namespace CanvasPlus
 {
-    //"When the context is created, the font of the
-    //context must be set to 10px sans-serif. "
-    LOGFONT logFont;
-    logFont.lfHeight = -10; //-17;
-    logFont.lfWidth = 0;
-    logFont.lfEscapement = 0;
-    logFont.lfOrientation = 0;
-    logFont.lfWeight = 400;
-    logFont.lfItalic = 0;
-    logFont.lfUnderline = 0;
-    logFont.lfStrikeOut = 0;
-    logFont.lfCharSet = 0;
-    logFont.lfOutPrecision = 3;
-    logFont.lfClipPrecision = 2;
-    logFont.lfQuality = 1;
-    logFont.lfPitchAndFamily = 49;
-    wcscpy(logFont.lfFaceName , L"sans-serif");
-    m_pNativeObject = (void*)CreateFontIndirect(&logFont);
-}
 
-CanvasPlus::Font::~Font()
-{
-    ::DeleteObject((HDC)m_pNativeObject);
-}
-
-CanvasPlus::Canvas::Canvas(void* p) : m_CanvasRenderingContext2D(p)
-{
-}
-
-CanvasPlus::Canvas::~Canvas()
-{
-}
-
-CanvasPlus::CanvasRenderingContext2D& CanvasPlus::Canvas::getContext(const char*)
-{
-    return m_CanvasRenderingContext2D;
-}
-
-CanvasPlus::CanvasRenderingContext2D::CanvasRenderingContext2D(void* p)
-    : m_pNativeHandle(p)
-{
-    //"When the context is created, the lineWidth attribute must initially have the value 1.0."
-    lineWidth = 1.0;
-}
-
-CanvasPlus::CanvasRenderingContext2D::~CanvasRenderingContext2D()
-{
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// The fillRect(x, y, w, h) method must paint the specified rectangular area
-//  using the fillStyle. If either height or width are zero, this method
-//  has no effect.
-void CanvasPlus::CanvasRenderingContext2D::fillRect(double x, double y, double w, double h)
-{
-    //------------------------------
-    HDC hdc = (HDC) m_pNativeHandle;
-    //-------------------------------
-    //TODO waiting for transformations..
-    RECT rect = {(LONG)x, (LONG)y, (LONG)(x + w), (LONG)(y + h)};
-    COLORREF color = RGB(fillStyle.m_Color.r, fillStyle.m_Color.g, fillStyle.m_Color.b);
-    FillSolidRect(hdc, &rect, color);
-}
-
-void CanvasPlus::CanvasRenderingContext2D::strokeRect(double x, double y, double w, double h)
-{
-    //------------------------------
-    HDC hdc = (HDC) m_pNativeHandle;
-    //-------------------------------
-    //TODO waiting for transformations..
-    RECT rect = {(LONG)x, (LONG)y, (LONG)(x + w), (LONG)(y + h)};
-    COLORREF color = RGB(fillStyle.m_Color.r, fillStyle.m_Color.g, fillStyle.m_Color.b);
-    //DrawRect(hdc, &rect, color);
-    HPEN hpen = CreatePen(PS_SOLID, lineWidth, color); //TODO
-    HPEN oldPen = (HPEN) SelectObject(hdc, hpen);
-    HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, GetStockObject(HOLLOW_BRUSH));
-    //
-    ::Rectangle(hdc, (LONG)x, (LONG)y, (LONG)(x + w), (LONG)(y + h));
-    //
-    SelectObject(hdc, oldPen);
-    SelectObject(hdc, oldBrush);
-}
-
-
-
-int CanvasPlus::CanvasRenderingContext2D::ToPixelX(double x)
-{
-    //TODO
-    return (int) x;
-}
-
-int CanvasPlus::CanvasRenderingContext2D::ToPixelY(double y)
-{
-    //TODO
-    return (int) y;
-}
-
-
-CanvasPlus::TextMetrics CanvasPlus::CanvasRenderingContext2D::measureText(const wchar_t* psz)
-{
-    //------------------------------
-    HDC hdc = (HDC) m_pNativeHandle;
-    //-------------------------------
-    //  TEXTMETRIC tm;
-    //  GetTextMetrics(hdc, &tm);
-    SIZE sz;
-    ::GetTextExtentPoint(hdc, psz, wcslen(psz), &sz);
-    return TextMetrics(sz.cx);
-}
-
-void CanvasPlus::CanvasRenderingContext2D::moveTo(double x, double y)
-{
-    //------------------------------
-    HDC hdc = (HDC) m_pNativeHandle;
-    //-------------------------------
-    const int ix = ToPixelX(x);
-    const int iy = ToPixelY(y);
-    ::MoveToEx(hdc, ix, iy, NULL);
-}
-
-void CanvasPlus::CanvasRenderingContext2D::lineTo(double x, double y)
-{
-    //------------------------------
-    HDC hdc = (HDC) m_pNativeHandle;
-    //-------------------------------
-    const int ix = ToPixelX(x);
-    const int iy = ToPixelY(y);
-    COLORREF color = RGB(fillStyle.m_Color.r, fillStyle.m_Color.g, fillStyle.m_Color.b);
-    HPEN hpen = CreatePen(PS_SOLID, lineWidth, color); //TODO
-    HPEN oldPen = (HPEN) SelectObject(hdc, hpen);
-    //
-    ::LineTo(hdc, ix, iy);
-    //
-    SelectObject(hdc, oldPen);
-}
-
-static void GdiTextOut(HDC hdc,
-                       CanvasPlus::TextAlign textAlign,
-                       CanvasPlus::TextBaselineEnum baseLine,
-                       const wchar_t* psz,
-                       int textlen,
-                       int ix,
-                       int iy)
-{
-    assert(GetTextAlign(hdc) == TA_LEFT);
-
-    SIZE sz;
-    ::GetTextExtentPointW(hdc, psz, textlen, &sz);
-
-    int newx = ix;
-    int newy = iy;
-
-    switch (textAlign)
+    struct CanvasGradientImp
     {
-    case CanvasPlus::TextAlignStart:  newx = ix;          break;
-    case CanvasPlus::TextAlignEnd:    newx = ix - sz.cx;  break;
-    case CanvasPlus::TextAlignLeft:   newx = ix;          break;
-    case CanvasPlus::TextAlignRight:  newx = ix - sz.cx;  break;
-    case CanvasPlus::TextAlignCenter: newx = ix - sz.cx / 2; break;
-    default:
-        assert(false);
+        COLORREF color1;
+        COLORREF color2;
+        double x0, y0, x1, y1;
+
+        unsigned int refcount;
+        
+
+        void Release()
+        {
+            refcount--;
+
+            if (refcount == 0)
+            {
+                delete this;
+            }
+        }
+
+        bool isVertical() const
+        {
+            return x0 == x1;
+        }
+
+        CanvasGradientImp(double x0, double y0, double x1, double y1)
+        {
+            this->x0 = x0;
+            this->y0 = y0;
+            this->x1 = x1;
+            this->y1 = y1;
+
+            refcount == 0;
+        }
+
+        void AddRef()
+        {
+            refcount++;
+        }
+
+        // opaque object
+        void addColorStop(double offset, const char* colortxt)
+        {
+            CanvasPlus::Color color(colortxt);
+
+            if (offset == 0)
+            {
+                color1 = RGB(color.r, color.g, color.b);
+            }
+            else if (offset == 1)
+            {
+                color2 = RGB(color.r, color.g, color.b);
+            }
+            else
+            {
+                assert(false);
+            }
+        }
+    };
+
+
+
+    void CanvasGradient::addColorStop(double offset, const char* color)
+    {
+        if (pCanvasGradientImp)
+        {
+            pCanvasGradientImp->addColorStop(offset, color);
+        }
     }
 
-    switch (baseLine)
+    CanvasGradient::CanvasGradient(CanvasGradientImp* p)
     {
-    case CanvasPlus::TextBaselineTop :
-        newy = iy;
-        break;//The top of the em square
+        this->pCanvasGradientImp = p;
+        this->pCanvasGradientImp->AddRef();
+    }
 
-    case CanvasPlus::TextBaselineHanging:
-        newy = iy;
-        break; //The hanging baseline
+    CanvasGradient::CanvasGradient(const CanvasGradient& other)
+    {
+        this->pCanvasGradientImp = other.pCanvasGradientImp;
+        this->pCanvasGradientImp->AddRef();
+    }
 
-    case CanvasPlus::TextBaselineMiddle :
-        newy = iy - sz.cy / 2;
-        break; //The middle of the em square
+    CanvasGradient::~CanvasGradient()
+    {
+        if (pCanvasGradientImp)
+        {
+            pCanvasGradientImp->Release();
+        }
+    }
 
-    case CanvasPlus::TextBaselineAlphabetic :
+    CanvasGradient Context2D::createLinearGradient(double x0,
+            double y0,
+            double x1,
+            double y1)
+    {
+        if (x0 == x1)
+        {
+            return CanvasGradient(new CanvasGradientImp(x0, y0, x1, y1));
+        }
+        else if (y0 == y1)
+        {
+            return CanvasGradient(new CanvasGradientImp(x0, y0, x1, y1));
+        }
+        else
+        {
+        }
+
+        return CanvasGradient();
+    }
+
+
+    Font::Font()
+    {
+        //"When the context is created, the font of the
+        //context must be set to 10px sans-serif. "
+        LOGFONT logFont;
+        logFont.lfHeight = -10; //-17;
+        logFont.lfWidth = 0;
+        logFont.lfEscapement = 0;
+        logFont.lfOrientation = 0;
+        logFont.lfWeight = 400;
+        logFont.lfItalic = 0;
+        logFont.lfUnderline = 0;
+        logFont.lfStrikeOut = 0;
+        logFont.lfCharSet = 0;
+        logFont.lfOutPrecision = 3;
+        logFont.lfClipPrecision = 2;
+        logFont.lfQuality = 1;
+        logFont.lfPitchAndFamily = 49;
+        wcscpy(logFont.lfFaceName , L"sans-serif");
+        m_pNativeObject = (void*)CreateFontIndirect(&logFont);
+    }
+
+    Font::~Font()
+    {
+        ::DeleteObject((HDC)m_pNativeObject);
+    }
+
+    Canvas::Canvas(void* p) : m_CanvasRenderingContext2D(p)
+    {
+    }
+
+    Canvas::~Canvas()
+    {
+    }
+
+    Context2D& Canvas::getContext(const char*)
+    {
+        return m_CanvasRenderingContext2D;
+    }
+
+    Context2D::Context2D(void* p)
+        : m_pNativeHandle(p)
+    {
+        //"When the context is created, the lineWidth attribute must initially have the value 1.0."
+        lineWidth = 1.0;
+    }
+
+    Context2D::~Context2D()
+    {
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // The fillRect(x, y, w, h) method must paint the specified rectangular area
+    //  using the fillStyle. If either height or width are zero, this method
+    //  has no effect.
+    void Context2D::fillRect(double x, double y, double w, double h)
+    {
+        //------------------------------
+        HDC hdc = (HDC) m_pNativeHandle;
+        //-------------------------------
+        //TODO waiting for transformations..
+        RECT rect = {(LONG)x, (LONG)y, (LONG)(x + w), (LONG)(y + h)};
+
+        if (fillStyle.fillStyleEnum == FillStyleEnumSolid)
+        {
+            COLORREF color = RGB(fillStyle.m_Color.r, fillStyle.m_Color.g, fillStyle.m_Color.b);
+            FillSolidRect(hdc, &rect, color);
+        }
+        else if (fillStyle.fillStyleEnum == FillStyleEnumGradient)
+        {
+            UINT mode = fillStyle.canvasGradient.pCanvasGradientImp->isVertical() ?
+                       GRADIENT_FILL_RECT_V: 
+                       GRADIENT_FILL_RECT_H;
+
+            COLORREF color1 = fillStyle.canvasGradient.pCanvasGradientImp->color1;
+            COLORREF color2 = fillStyle.canvasGradient.pCanvasGradientImp->color2;             
+
+            if (mode == GRADIENT_FILL_RECT_V && 
+                fillStyle.canvasGradient.pCanvasGradientImp->y0 >
+                fillStyle.canvasGradient.pCanvasGradientImp->y1)
+            {
+                std::swap(color1, color2);
+            }
+
+            if (mode == GRADIENT_FILL_RECT_H && 
+                fillStyle.canvasGradient.pCanvasGradientImp->x0 >
+                fillStyle.canvasGradient.pCanvasGradientImp->x1)
+            {
+                std::swap(color1, color2);
+            }
+
+
+            FillGradientRect(hdc, rect, color1,color2, mode);
+        }
+    }
+
+    void Context2D::strokeRect(double x, double y, double w, double h)
+    {
+        //------------------------------
+        HDC hdc = (HDC) m_pNativeHandle;
+        //-------------------------------
+        //TODO waiting for transformations..
+        RECT rect = {(LONG)x, (LONG)y, (LONG)(x + w), (LONG)(y + h)};
+        COLORREF color = RGB(strokeStyle.m_Color.r, strokeStyle.m_Color.g, strokeStyle.m_Color.b);
+        //DrawRect(hdc, &rect, color);
+        HPEN hpen = CreatePen(PS_SOLID, lineWidth, color); //TODO
+        HPEN oldPen = (HPEN) SelectObject(hdc, hpen);
+        HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, GetStockObject(HOLLOW_BRUSH));
+        //
+        ::Rectangle(hdc, (LONG)x, (LONG)y, (LONG)(x + w), (LONG)(y + h));
+        //
+        SelectObject(hdc, oldPen);
+        SelectObject(hdc, oldBrush);
+    }
+
+
+
+    int Context2D::ToPixelX(double x)
+    {
+        //TODO
+        return (int) x;
+    }
+
+    int Context2D::ToPixelY(double y)
+    {
+        //TODO
+        return (int) y;
+    }
+
+
+    TextMetrics Context2D::measureText(const wchar_t* psz)
+    {
+        //------------------------------
+        HDC hdc = (HDC) m_pNativeHandle;
+        //-------------------------------
+        //  TEXTMETRIC tm;
+        //  GetTextMetrics(hdc, &tm);
+        SIZE sz;
+        ::GetTextExtentPoint(hdc, psz, wcslen(psz), &sz);
+        return TextMetrics(sz.cx);
+    }
+
+    void Context2D::moveTo(double x, double y)
+    {
+        //------------------------------
+        HDC hdc = (HDC) m_pNativeHandle;
+        //-------------------------------
+        const int ix = ToPixelX(x);
+        const int iy = ToPixelY(y);
+        ::MoveToEx(hdc, ix, iy, NULL);
+    }
+
+    void Context2D::lineTo(double x, double y)
+    {
+        //------------------------------
+        HDC hdc = (HDC) m_pNativeHandle;
+        //-------------------------------
+        const int ix = ToPixelX(x);
+        const int iy = ToPixelY(y);
+        COLORREF color = RGB(fillStyle.m_Color.r, fillStyle.m_Color.g, fillStyle.m_Color.b);
+        HPEN hpen = CreatePen(PS_SOLID, lineWidth, color); //TODO
+        HPEN oldPen = (HPEN) SelectObject(hdc, hpen);
+        //
+        ::LineTo(hdc, ix, iy);
+        //
+        SelectObject(hdc, oldPen);
+    }
+
+    static void GdiTextOut(HDC hdc,
+                           TextAlign textAlign,
+                           TextBaselineEnum baseLine,
+                           const wchar_t* psz,
+                           int textlen,
+                           int ix,
+                           int iy)
+    {
+        assert(GetTextAlign(hdc) == TA_LEFT);
+        SIZE sz;
+        ::GetTextExtentPointW(hdc, psz, textlen, &sz);
+        int newx = ix;
+        int newy = iy;
+
+        switch (textAlign)
+        {
+        case TextAlignStart:
+            newx = ix;
+            break;
+
+        case TextAlignEnd:
+            newx = ix - sz.cx;
+            break;
+
+        case TextAlignLeft:
+            newx = ix;
+            break;
+
+        case TextAlignRight:
+            newx = ix - sz.cx;
+            break;
+
+        case TextAlignCenter:
+            newx = ix - sz.cx / 2;
+            break;
+
+        default:
+            assert(false);
+        }
+
+        switch (baseLine)
+        {
+        case TextBaselineTop :
+            newy = iy;
+            break;//The top of the em square
+
+        case TextBaselineHanging:
+            newy = iy;
+            break; //The hanging baseline
+
+        case TextBaselineMiddle :
+            newy = iy - sz.cy / 2;
+            break; //The middle of the em square
+
+        case TextBaselineAlphabetic :
         {
             TEXTMETRIC tm;
             GetTextMetrics(hdc, &tm);
@@ -208,7 +420,7 @@ static void GdiTextOut(HDC hdc,
         }
         break;//The alphabetic baseline
 
-    case CanvasPlus::TextBaselineIdeographic :
+        case TextBaselineIdeographic :
         {
             TEXTMETRIC tm;
             GetTextMetrics(hdc, &tm);
@@ -216,34 +428,36 @@ static void GdiTextOut(HDC hdc,
         }
         break; //The ideographic baseline
 
-    case CanvasPlus::TextBaselineBottom :
-        newy = iy - sz.cy;
-        break;//The bottom of the em square
+        case TextBaselineBottom :
+            newy = iy - sz.cy;
+            break;//The bottom of the em square
 
-    default:
-        break;
+        default:
+            break;
+        }
+
+        TextOut(hdc, newx, newy, psz, wcslen(psz));
     }
 
-    TextOut(hdc, newx, newy, psz, wcslen(psz));
-}
+    void Context2D::fillText(const wchar_t* psz, double x, double y)
+    {
+        //------------------------------
+        HDC hdc = (HDC) m_pNativeHandle;
+        //-------------------------------
+        const int ix = ToPixelX(x);
+        const int iy = ToPixelY(y);
+        //
+        COLORREF color = RGB(fillStyle.m_Color.r, fillStyle.m_Color.g, fillStyle.m_Color.b);
+        COLORREF oldcolor = SetTextColor(hdc, color);
+        int oldbkmode = SetBkMode(hdc, TRANSPARENT);
+        HFONT oldfont = (HFONT) SelectObject(hdc, (HFONT)font.m_pNativeObject);
+        //
+        GdiTextOut(hdc, this->textAlign, this->textBaseline, psz, wcslen(psz), ix, iy);
+        //
+        SetTextColor(hdc, oldcolor);
+        SetBkMode(hdc, oldbkmode);
+        SelectObject(hdc, oldfont);
+    }
 
-void CanvasPlus::CanvasRenderingContext2D::fillText(const wchar_t* psz, double x, double y)
-{
-    //------------------------------
-    HDC hdc = (HDC) m_pNativeHandle;
-    //-------------------------------
-    const int ix = ToPixelX(x);
-    const int iy = ToPixelY(y);
-    //
-    COLORREF color = RGB(fillStyle.m_Color.r, fillStyle.m_Color.g, fillStyle.m_Color.b);
-    COLORREF oldcolor = SetTextColor(hdc, color);
-    int oldbkmode = SetBkMode(hdc, TRANSPARENT);
-    HFONT oldfont = (HFONT) SelectObject(hdc, (HFONT)font.m_pNativeObject);
-    //
-    GdiTextOut(hdc, this->textAlign, this->textBaseline, psz, wcslen(psz), ix, iy);
-    //
-    SetTextColor(hdc, oldcolor);
-    SetBkMode(hdc, oldbkmode);
-    SelectObject(hdc, oldfont);
-}
+} //namespace CanvasPlus
 
